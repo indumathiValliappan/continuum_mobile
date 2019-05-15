80b42edf0acf52a2d5ddfe5151819e7363f223de
Aishwarya <aishwarya@localhost.localdomain>
2019-05-15 14:06:26 +0530

    Introduced API Call instead of hardcoded values

--- flutter_app_ctm/lib/ProgressionListPage.dart
+++ flutter_app_ctm/lib/ProgressionListPage.dart
@@ -1,11 +1,21 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_app_ctm/ProgressionsList.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
-class ProgressionListPage extends StatelessWidget {
+class ProgressionListPage extends StatefulWidget {
+  @override
+  ProgListPage createState() => new ProgListPage();
+}
+
+class ProgListPage extends State<ProgressionListPage> {
+  List<String> prog = new List();
   @override
   Widget build(BuildContext context) {
     final title = 'Progressions';
-
+    fetchProgressionList();
     return MaterialApp(
       title: title,
       home: Scaffold(
@@ -14,26 +24,56 @@ class ProgressionListPage extends StatelessWidget {
               'images/v1-logo.png'),
           title: Text(title),
         ),
-        body: ListView(
-          children: <Widget>[
-            ListTile(
-              title: Text('prog1'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) =>  Progressions()),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('prog2'),
-            ),
-            ListTile(
-              title: Text('prog3'),
-            ),
-          ],
+        body: new ListView.builder(
+            itemCount: this.prog == null ? 0 : this.prog.length,
+            itemBuilder: (BuildContext context, int index) {
+              return new Container(
+                  child: new Center(
+                  child: new Column(
+                      children: <Widget>[
+                        ListTile(
+                          title: Text(this.prog[index]),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Progressions()),
+                            );
+                          },
+                        ),
+                        ]
+                  )
+                  )
+              );
+            }
         ),
       ),
     );
+  }
+
+    Future<String> fetchProgressionList() async {
+    final prefs = await SharedPreferences.getInstance();
+    final instanceUrl = prefs.get("instanceUrl");
+    final token = prefs.get("token");
+    final response = await http.get(
+        instanceUrl+':8080/api/list_progressions',
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Token "+token
+        });
+    Iterable data = json.decode(response.body)['Response'];
+    List<String> progression = new List();
+    for (var prog in data) {
+      progression.add(prog["name"]);
+    }
+    setState(() {
+      this.prog = progression;
+    });
+    return "Success";
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
   }
 }
